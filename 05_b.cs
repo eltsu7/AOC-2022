d5da53219fef9b7dcb792b6aa7e070a02907e2bc
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2022-12-05 16:46:58 +0200

    Update 05_b.cs

--- 05_b.cs
+++ 05_b.cs
@@ -1,66 +1,57 @@
-using System;
-using System.Collections.Generic;
-
-namespace aoc_05
+static void moi()
 {
-    class Program
+    const string path = "C:/Users/eeli.hernesniemi/source/repos/aoc_05/aoc_05/input.txt";
+    string[] lines = System.IO.File.ReadAllLines(path);
+
+    var stacks = new List<string>();
+    int num_of_stacks = (lines[0].Length / 4) + 1;
+    for (int num = 0; num < num_of_stacks; num++)
     {
-        static void Main(string[] args)
+        stacks.Add("");
+    }
+
+    int max_stack = 0;
+    while (lines[max_stack][1] != '1')
+    {
+        max_stack++;
+    }
+
+    for (int i = max_stack - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < num_of_stacks; j++)
         {
-            var watch = new System.Diagnostics.Stopwatch();
-            watch.Start();
-
-            const string path = "C:/Users/eeli.hernesniemi/source/repos/aoc_05/aoc_05/input.txt";
-            string[] lines = System.IO.File.ReadAllLines(path);
-
-            var stacks = new List<Stack<char>>();
-            int num_of_stacks = (lines[0].Length / 4) + 1;
-            for (int num = 0; num < num_of_stacks; num++)
+            if (lines[i][(j * 4) + 1] != ' ')
             {
-                stacks.Add(new Stack<char>());
+                stacks[j] += lines[i][(j * 4) + 1];
             }
-
-            int max_stack = 0;
-            while (lines[max_stack][1] != '1')
-            {
-                max_stack++;
-            }
-
-            for (int i = max_stack - 1; i >= 0; i--)
-            {
-                for (int j = 0; j < num_of_stacks; j++)
-                {
-                    if (lines[i][(j * 4) + 1] != ' ')
-                    {
-                        stacks[j].Push(lines[i][(j * 4) + 1]);
-                    }
-                }
-            }
-
-            var temp_list = new List<char>();
-            for (int i = max_stack + 2; i < lines.Length; i++)
-            {
-                string[] line = lines[i].Split(' ');
-                for (int j = 0; j < int.Parse(line[1]); j++)
-                {
-                    temp_list.Add(stacks[int.Parse(line[3]) - 1].Pop());
-                }
-                temp_list.Reverse();
-                foreach (var num in temp_list)
-                {
-                    stacks[int.Parse(line[5]) - 1].Push(num);
-                }
-                temp_list.Clear();
-            }
-
-            foreach (var stack in stacks)
-            {
-                Console.Write(stack.Peek());
-            }
-            watch.Stop();
-            Console.WriteLine();
-            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
-            Console.ReadKey();
         }
     }
+
+    string temp;
+    int count;
+    int from;
+    int to;
+    for (int i = max_stack + 2; i < lines.Length; i++)
+    {
+        string[] line = lines[i].Split(' ');
+        count = int.Parse(line[1]);
+        from = int.Parse(line[3]) - 1;
+        to = int.Parse(line[5]) - 1;
+
+        temp = stacks[from].Substring(stacks[from].Length - count);
+
+        stacks[from] = stacks[from].Remove(stacks[from].Length - count);
+        stacks[to] += temp;
+    }
 }
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+for (int i = 0; i < 100000; i++)
+{
+    moi();
+}
+watch.Stop();
+Console.WriteLine();
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+Console.ReadKey();
