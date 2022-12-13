bbd5e9a9c55029ef53afe9d9d74804f0254c0287
Eeli Hernesniemi <eeli.hernesniemi@atostek.com>
2022-12-13 15:10:02 +0200

    13 wip

---
+++ 13_a/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+var lines = System.IO.File.ReadAllLines("input.txt").ToList();
+
+JsonElement asd = JsonSerializer.Deserialize<JsonElement>(lines[0]);
+
+var packets = new List<JsonElement>();
+
+
+bool CheckPackets(JsonElement left, JsonElement right)
+{
+    Console.WriteLine(a < b);
+    return true;
+}
+
+for (int i = 0; i < lines.Count; i++)
+{
+    if (lines[i] == "")
+    {
+        continue;
+    }
+    packets.Add(JsonSerializer.Deserialize<JsonElement>(lines[i]));
+
+    if (packets.Count == 2)
+    {
+        CheckPackets(packets[0], packets[1]);
+    }
+}
