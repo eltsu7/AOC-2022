a96f850a0fccac95e41148933bbba9a0bffc3f46
Eeli Hernesniemi <eeli.hernesniemi@atostek.com>
2022-12-13 16:46:39 +0200

    wip

--- 13_a/Program.cs
+++ 13_a/Program.cs
@@ -7,11 +7,44 @@ JsonElement asd = JsonSerializer.Deserialize<JsonElement>(lines[0]);
 var packets = new List<JsonElement>();
 
 
-bool CheckPackets(JsonElement left, JsonElement right)
+bool? IsInOrder(JsonElement left, JsonElement right)
 {
-    Console.WriteLine(a < b);
-    return true;
+    if (left.ValueKind == JsonValueKind.Array && right.ValueKind == JsonValueKind.Array)
+    {
+        for (int i = 0; i < Math.Min(left.GetArrayLength(), right.GetArrayLength()); i++)
+        {
+            
+        }
+        return left.GetArrayLength() < right.GetArrayLength();
+    }
+    else if (left.ValueKind == JsonValueKind.Number && right.ValueKind == JsonValueKind.Number)
+    {
+        return left.GetInt16() == right.GetInt16() ? null : (left.GetInt16() < right.GetInt16());
+    }
+    else
+    {
+        var asd = new JsonElement();
+    }
+
+    return null;
 }
+
+/*
+If both values are integers, the lower integer should come first. 
+If the left integer is lower than the right integer, the inputs are in the right order. 
+If the left integer is higher than the right integer, the inputs are not in the right order. 
+Otherwise, the inputs are the same integer; continue checking the next part of the input.
+
+
+If both values are lists, compare the first value of each list, then the second value, and so on. 
+If the left list runs out of items first, the inputs are in the right order. 
+If the right list runs out of items first, the inputs are not in the right order. 
+If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+
+
+If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison. 
+For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2); the result is then found by instead comparing [0,0,0] and [2].
+*/
 
 for (int i = 0; i < lines.Count; i++)
 {
@@ -23,6 +56,6 @@ for (int i = 0; i < lines.Count; i++)
 
     if (packets.Count == 2)
     {
-        CheckPackets(packets[0], packets[1]);
+        IsInOrder(packets[0], packets[1]);
     }
 }
